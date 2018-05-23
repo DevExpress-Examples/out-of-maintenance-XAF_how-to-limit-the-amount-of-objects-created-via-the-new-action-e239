01d20eca7a0e90fc436d2af8906dfcb4a482331b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/HowToLimitAmountOfObjectsCreatedViaNewAction.Module.Web/WebModule.vb
@@ -0,0 +1,15 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace LimitObjectsCreatedViaNewAction.Module.Web
+	<ToolboxItemFilter("Xaf.Platform.Web")> _
+	Public NotInheritable Partial Class LimitObjectsCreatedViaNewActionAspNetModule
+		Inherits ModuleBase
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
