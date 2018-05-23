49e607b6854101254c738c0453077fd8bf9919b7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToLimitAmountOfObjectsCreatedViaNewAction.Module.Win/WinModule.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace LimitObjectsCreatedViaNewAction.Module.Win {
+	[ToolboxItemFilter("Xaf.Platform.Win")]
+	public sealed partial class LimitObjectsCreatedViaNewActionWindowsFormsModule : ModuleBase {
+		public LimitObjectsCreatedViaNewActionWindowsFormsModule() {
+			InitializeComponent();
+		}
+	}
+}
