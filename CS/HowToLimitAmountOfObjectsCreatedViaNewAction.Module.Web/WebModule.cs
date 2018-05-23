7a39b108a371d24e6194f44db528dcb3fb276939
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToLimitAmountOfObjectsCreatedViaNewAction.Module.Web/WebModule.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace LimitObjectsCreatedViaNewAction.Module.Web {
+	[ToolboxItemFilter("Xaf.Platform.Web")]
+	public sealed partial class LimitObjectsCreatedViaNewActionAspNetModule : ModuleBase {
+		public LimitObjectsCreatedViaNewActionAspNetModule() {
+			InitializeComponent();
+		}
+	}
+}
